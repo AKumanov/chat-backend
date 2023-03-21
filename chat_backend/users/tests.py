1b1d5fd8e9cdb2a171cfbfd41f3677cd97f2b343
Alexander Kumanov <alexander.kumanov@dentap.local>
2023-03-21 17:34:07 +0200

    broken test

--- chat_backend/users/tests.py
+++ chat_backend/users/tests.py
@@ -11,4 +11,4 @@ class UserTest(TestCase):
             name="Alexander",
         )
         user.save()
-        self.assertEqual("Alexander", user.name)
+        self.assertEqual("Test Name", user.name)
